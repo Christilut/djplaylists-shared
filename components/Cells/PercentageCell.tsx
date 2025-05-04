65482d8b846828611b761862762b7bdabc8ba723
Christiaan Maks <christiaan@rekord.cloud>
2025-05-04 22:01:39 +0200

    cell

--- components/Cells/PercentageCell.tsx
+++ components/Cells/PercentageCell.tsx
@@ -2,8 +2,8 @@ import { cn } from '../../../../lib/utils';
 
 const getPercentageColor = (value: number): { bg: string; text: string } => {
   if (value <= 35) return { bg: 'bg-red-500/60', text: 'text-white' }
-  else if (value >= 70) return { bg: 'bg-green-300/60', text: 'text-white' }
-  else return { bg: 'bg-yellow-300/60', text: 'text-white' }
+  else if (value >= 70) return { bg: 'bg-green-500/60', text: 'text-white' }
+  else return { bg: 'bg-yellow-400/60', text: 'text-white' }
 };
 
 const PercentageCell = ({ value, className }: { value: number | undefined, className?: string }) => {
@@ -14,7 +14,7 @@ const PercentageCell = ({ value, className }: { value: number | undefined, class
 
   return (
     <div className={cn("text-center", className)}>
-      <div className={cn("inline-block px-2 py-1 rounded-md", bg, text)}>
+      <div className={cn("inline-block px-3 py-1 rounded-full", bg, text)}>
         {percentage}%
       </div>
     </div>
