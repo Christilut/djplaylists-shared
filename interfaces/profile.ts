aea46958edabfd8713b033eaf088a50328042377
Christiaan Maks <christiaan@rekord.cloud>
2025-05-11 09:14:22 +0200

    tags

---
+++ interfaces/profile.ts
@@ -0,0 +1,8 @@
+export const DJ_TYPES = [
+  'Bedroom DJ',
+  'Wedding DJ',
+  'Open Format DJ',
+  'Festival DJ',
+  'Club DJ',
+  'Radio DJ'
+]
---
+++ interfaces/tags.ts
@@ -0,0 +1,49 @@
+
+export const PLAYLIST_TAG_OPTIONS = [
+  'Harmonic',
+  'Mixable',
+  'Steady BPM',
+  'Varied BPM',
+  // 'Vibe Set',
+
+  // 
+  'Classic',
+  'Old School',
+  'Throwback',
+  '70s',
+  '80s',
+  '90s',
+  '00s',
+
+  // Timing
+  'Warmup',
+  'Pre-Party',
+  'Filler',
+  'Peak Hour',
+  'Outro',
+  'Handoff',
+  'Closer',
+  'Afterhours',
+  'After-afterhours',
+
+  // Mood
+  'Aggressive',
+  'Calm',
+  'Chill',
+  'Dark',
+  'Euphoric',
+  'Feel Good',
+  'Funny',
+  'Funky',
+  'Groovy',
+  'Happy',
+  'Party',
+  'Sad',
+  'Scary',
+  'Mellow',
+  'Romantic',
+  'Unwind',
+  'Upbeat',
+  'Uplifting',
+  'Warm',
+];
