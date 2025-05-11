0d20e8e7679e3b94a18d2eb192bfdc38fae6b185
Christiaan Maks <christiaan@rekord.cloud>
2025-05-11 14:23:42 +0200

    tags

--- interfaces/tags.ts
+++ interfaces/tags.ts
@@ -1,49 +1,55 @@
 
 export const PLAYLIST_TAG_OPTIONS = [
-  'Harmonic',
-  'Mixable',
-  'Steady BPM',
-  'Varied BPM',
-  // 'Vibe Set',
-
-  // 
-  'Classic',
-  'Old School',
-  'Throwback',
-  '70s',
-  '80s',
-  '90s',
-  '00s',
-
-  // Timing
-  'Warmup',
-  'Pre-Party',
-  'Filler',
-  'Peak Hour',
-  'Outro',
-  // 'Handoff',
-  'Closer',
-  'Afterhours',
-  // 'After-afterhours',
-
-  // Mood
-  'Aggressive',
-  // 'Calm',
-  'Chill',
-  'Dark',
-  'Euphoric',
-  'Feel Good',
-  // 'Funny',
-  // 'Funky',
-  // 'Groovy',
-  'Happy',
-  // 'Party',
-  'Sad',
-  // 'Scary',
-  // 'Mellow',
-  'Romantic',
-  'Unwind',
-  'Upbeat',
-  // 'Uplifting',
-  // 'Warm',
+  {
+    category: 'Mixability',
+    tags: [
+      'Harmonic',
+      'Mixable',
+      'Steady BPM',
+      'Varied BPM',
+    ]
+  },
+  {
+    category: 'Decade',
+    tags: [
+      'Classic',
+      'Old School',
+      'Throwback',
+      '70s',
+      '80s',
+      '90s',
+      '00s',
+      '10s',
+    ]
+  },
+  {
+    category: 'Timing',
+    tags: [
+      'Warmup',
+      'Pre-Party',
+      'Filler',
+      'Peak Hour',
+      'Outro',
+      'Closer',
+      'Afterhours',
+    ]
+  },
+  {
+    category: 'Mood',
+    tags: [
+      'Aggressive',
+      'Chill',
+      'Dark',
+      'Euphoric',
+      'Feel Good',
+      'Funny',
+      'Groovy',
+      'Happy',
+      'Sad',
+      'Romantic',
+      'Unwind',
+      'Upbeat',
+      'Uplifting',
+    ]
+  },
 ];
